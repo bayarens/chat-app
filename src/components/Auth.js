3269e25c6146180d852e74902779aa3e8f2a4704
Bayley.Arens <bayley.arens@fdmgroup.com>
2023-02-15 20:00:33 -0500

    cookies

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,10 +1,15 @@
 import { auth, provider } from '../firebase-config'
 import { signInWithPopup } from 'firebase/auth'
 
+import Cookies from 'universal-cookie'
+
+const cookies = new Cookies();
+
 export const Auth = () => {
 
     const signInWithGoogle = async () =>{
-        await signInWithPopup(auth, provider)
+      const result = await signInWithPopup(auth, provider);
+      cookies.set("auth-token", result.user.refreshToken);
     }
 
     return <div className="auth"> 
