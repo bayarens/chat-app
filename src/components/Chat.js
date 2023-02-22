2181fddd9b2adfecdd2bc0804fd6acdc404cc461
Bayley.Arens <bayley.arens@fdmgroup.com>
2023-02-21 19:39:55 -0500

    messages UI and ordered

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,62 +1,76 @@
 import { useEffect, useState } from "react";
-import { addDoc, collection, serverTimestamp, onSnapshot, query, where } from "firebase/firestore"
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  onSnapshot,
+  query,
+  where,
+  orderBy,
+} from "firebase/firestore";
 import { auth, db } from "../firebase-config";
-import '../styles/Chat.css' 
+import "../styles/Chat.css";
 
 export const Chat = (props) => {
+  const { room } = props;
 
-    const { room } = props;
+  const [messages, setMessages] = useState([]);
 
-    const [messages, setMessages] = useState([]);
-    
-    const [newMessage, setNewMessage] = useState(""); 
+  const [newMessage, setNewMessage] = useState("");
 
-    const messageRef = collection(db, "messages");
+  const messageRef = collection(db, "messages");
 
+  //this function receives messages automatically, by listening to changes that happen in the collection
+  useEffect(() => {
+    const queryMessages = query(messageRef, where("room", "==", room), orderBy("createdAt"));
+    const unsusscribe = onSnapshot(queryMessages, (snapshot) => {
+      let messages = [];
+      snapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      console.log(messages);
+      setMessages(messages);
+    });
 
-    //this function receives messages automatically, by listening to changes that happen in the collection 
-    useEffect(() => {
-      const queryMessages = query(messageRef, where("room", "==", room)); 
-      const unsusscribe = onSnapshot(queryMessages, (snapshot) => {
-        let messages = [];
-        snapshot.forEach((doc) => {
-          messages.push({...doc.data(), id: doc.id})
-        })
-        console.log(messages)
-        setMessages(messages)
-      })
+    return () => unsusscribe();
+  }, []);
 
-      return () => unsusscribe()
-    }, []);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (newMessage === "") return;
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        if(newMessage === "") return; 
-        
-        //adding document to firestore database 
+    //adding document to firestore database
 
-        await addDoc(messageRef, {
-          text: newMessage,
-          createdAt: serverTimestamp(),
-          user: auth.currentUser.displayName,
-          room,
-        }); 
-        
-        //clearing out new message state for a new message later
-        setNewMessage("");
-        
-    };
+    await addDoc(messageRef, {
+      text: newMessage,
+      createdAt: serverTimestamp(),
+      user: auth.currentUser.displayName,
+      room,
+    });
+
+    //clearing out new message state for a new message later
+    setNewMessage("");
+  };
 
   return (
     <div className="chat-app">
-    <div> {messages.map((message) => <h1>{message.text}</h1>)} </div>
+      <div className="header">
+        <h1>Welcome to: {room.toUpperCase()}</h1>
+      </div>
+      <div className="messages">
+        {messages.map((message) => (
+          <div key={message.id} className="message">
+            <span className="user">{message.user}:</span> {message.text}
+          </div>
+        ))}
+      </div>
       <form onSubmit={handleSubmit} className="new-message-form">
-        <input 
-            className="new-message-input" 
-            placeholder="Type here..." 
-            onChange={(e) => setNewMessage(e.target.value)}
-            value={newMessage}    
-            />
+        <input
+          className="new-message-input"
+          placeholder="Type here..."
+          onChange={(e) => setNewMessage(e.target.value)}
+          value={newMessage}
+        />
         <button type="submit" className="send-button">
           Send
         </button>
