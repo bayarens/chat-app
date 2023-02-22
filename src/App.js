0053d82f70857c3b48d61d87521b3e3cec833547
Bayley.Arens <bayley.arens@fdmgroup.com>
2023-02-21 19:58:38 -0500

    sign-out function

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import "./App.css";
 import { Auth } from "./components/Auth";
 import Cookies from "universal-cookie";
 import { Chat } from "./components/Chat";
+import {signOut} from "firebase/auth";
+import {auth} from "./firebase-config"
+
 const cookies = new Cookies();
+
 
 function App() {
   const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
   const [room, setRoom] = useState(null);
 
   const roomInputRef = useRef(null);
+
+  const signUserOut = async  () => {
+      await signOut(auth)
+      cookies.remove("auth-token")
+      setIsAuth(false)
+      setRoom(null)
+  }
 
   if (!isAuth) {
     return (
@@ -20,6 +31,7 @@ function App() {
   }
 
   return (
+    <>
     <div className="container">
       {room ? (
         <Chat room={room}/>
@@ -33,6 +45,10 @@ function App() {
         </div>
       )}
     </div>
+    <div className="sign-out">
+          <button className="sign-out-btn" onClick={signUserOut}> Sign Out</button>
+    </div>
+    </>
   );
 }
 
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,5 +1,6 @@
 import { auth, provider } from '../firebase-config'
 import { signInWithPopup } from 'firebase/auth'
+import "../styles/Auth.css"
 
 import Cookies from 'universal-cookie'
 
